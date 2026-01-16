f5b2692c283ed486cfef39cb129429c1c52b75ab
Muhammad Rehman <zakoota78@gamil.com>
2026-01-17 00:06:59 +0500

    Build to docs folder for GitHub Pages - fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/personal-portfolio-website/',
   build: {
+    outDir: 'docs',
     sourcemap: false,
   },
   plugins: [
